98c76a765951c805e49bddcf998a2515953cfd13
Andrés Mejía <amejia@uamv.edu.ni>
2026-09-06 19:25:22 -0600

    "feat:Clase Pelicula y Registro-Peliculas.fxml modificados"

--- src/main/java/com/appregistros/appregistros/controller/PeliculaController.java
+++ src/main/java/com/appregistros/appregistros/controller/PeliculaController.java
@@ -1,4 +1,11 @@
 package com.appregistros.appregistros.controller;
 
+import javafx.fxml.FXML;
+
 public class PeliculaController {
+    @FXML
+    private void initialize() {
+
+    }
+
 }
--- src/main/java/model/Peliculas.java
+++ src/main/java/model/Peliculas.java
@@ -1,4 +1,60 @@
 package model;
 
 public class Peliculas {
+    private String Nombre;
+    private String Director;
+    private String FechaEstreno;
+    private String Genero;
+    private String Duracion;
+
+    public Peliculas() {
+    }
+
+    public Peliculas(String nombre, String director, String fechaEstreno, String genero, String duracion) {
+        Nombre = nombre;
+        Director = director;
+        FechaEstreno = fechaEstreno;
+        Genero = genero;
+        Duracion = duracion;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getDirector() {
+        return Director;
+    }
+
+    public void setDirector(String director) {
+        Director = director;
+    }
+
+    public String getFechaEstreno() {
+        return FechaEstreno;
+    }
+
+    public void setFechaEstreno(String fechaEstreno) {
+        FechaEstreno = fechaEstreno;
+    }
+
+    public String getGenero() {
+        return Genero;
+    }
+
+    public void setGenero(String genero) {
+        Genero = genero;
+    }
+
+    public String getDuracion() {
+        return Duracion;
+    }
+
+    public void setDuracion(String duracion) {
+        Duracion = duracion;
+    }
 }
